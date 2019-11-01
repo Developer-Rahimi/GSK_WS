f4773d8f4f05c704cb5325174d1d20ea35a739e3
Developer-Rahimi <moosarahimi8@gmail.com>
2019-11-01 23:32:06 +0330

    add State Controller

---
+++ wherapp_gsk/Controllers/SteteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using wherapp_gsk.Services;
+
+namespace wherapp_gsk.Controllers
+{
+    public class SteteController : ApiController
+    {
+        private DatabaseContext db = new DatabaseContext();
+        public HttpResponseMessage Get()
+        {
+            var data = db.States.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+    }
+}
