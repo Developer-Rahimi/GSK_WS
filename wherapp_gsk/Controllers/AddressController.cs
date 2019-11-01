89fba59fabac2abec891367c8aaf1d53aadbfdca
Developer-Rahimi <moosarahimi8@gmail.com>
2019-11-01 22:41:07 +0330

    add methode get address

--- wherapp_gsk/Controllers/AddressController.cs
+++ wherapp_gsk/Controllers/AddressController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using wherapp_gsk.Services;
 
 namespace wherapp_gsk.Controllers
 {
     public class AddressController : ApiController
     {
+        private DatabaseContext db = new DatabaseContext();
+        public HttpResponseMessage Get()
+        {
+            //var Carts = db.Carts.Include(x => x.Content).ToList();
+            /* .Include(x => x.User)*/
+            return Request.CreateResponse(HttpStatusCode.OK, "Ok");
+        }
     }
 }
