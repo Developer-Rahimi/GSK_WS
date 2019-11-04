05f9f0b5cd753ca2b47e8eef4f4214b40b8d17d1
Developer-Rahimi <moosarahimi8@gmail.com>
2019-11-04 23:34:29 +0330

    get city and post

---
+++ wherapp_gsk/Controllers/CityController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using wherapp_gsk.Models;
+using wherapp_gsk.Services;
+
+namespace wherapp_gsk.Controllers
+{
+    public class CityController : ApiController
+    {
+        private DatabaseContext db = new DatabaseContext();
+        public HttpResponseMessage Get(int index)
+        {
+            var data = db.States.Where(x=>x.StateID==index).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        public HttpResponseMessage Post([FromBody] City city)
+        {
+            Result result = new Result();
+            db.Cities.Add(city);
+            db.SaveChanges();
+            result.Status = "OK";
+            return Request.CreateResponse(HttpStatusCode.OK, result);
+        }
+    }
+}
