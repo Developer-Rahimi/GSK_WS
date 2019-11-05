ebfd81dfc1269d016ee35bcb055d4bb253238695
Developer-Rahimi <moosarahimi8@gmail.com>
2019-11-05 23:42:58 +0330

    add post address

--- wherapp_gsk/Controllers/AddressController.cs
+++ wherapp_gsk/Controllers/AddressController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using wherapp_gsk.Models;
 using wherapp_gsk.Services;
 
 namespace wherapp_gsk.Controllers
@@ -16,6 +17,14 @@ namespace wherapp_gsk.Controllers
             var data = db.addresses.ToList();
             /* .Include(x => x.User)*/
             return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+        public HttpResponseMessage Post([FromBody] Address address)
+        {
+            Result result = new Result();
+            result.Status = "OK";
+            db.addresses.Add(address);
+            db.SaveChanges();
+            return Request.CreateResponse(HttpStatusCode.OK, result);
         }
     }
 }
