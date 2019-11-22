1c30be6bec8ca683d0fc45b21eb1b08223f52000
Developer-Rahimi <moosarahimi8@gmail.com>
2019-11-22 13:14:55 +0330

    update product model

--- wherapp_gsk/Models/Product.cs
+++ wherapp_gsk/Models/Product.cs
@@ -9,14 +9,14 @@ namespace wherapp_gsk.Models
     public class Product
     {
         public int id { get; set; }
-        public Content Content { get; set; }
+
+        public string ProductName { get; set; }
+
         public long ContentID { get; set; }
-        public Category Category { get; set; }
-        public int CategoryID { get; set; }
-        public int? SubCategoryID { get; set; }
-        public decimal ProductPrice { get; set; }
-        public int ProductPriceUnit { get; set; }
-        public int ProductReview { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public decimal? ProductPrice { get; set; }
 
     }
 }
